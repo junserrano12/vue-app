0943b042d8185e25b37bf615d350e3b73d99487a
dwh.jun.serrano <salvador.serrano@directwithhotels.com>
2019-01-10 18:55:55 +0800

    add vbs, bat

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require('express')
+// const serveStatic = require('serve-static')
 const path = require('path')
 
-const history = require('connect-history-api-fallback');
+// create the express app
+const app = express()
+// create middleware to handle the serving the app
+// app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+app.use(express.static('dist'));
 
-const app = express();
+app.get('*', function(req, res) {
+  res.sendFile( path.join (__dirname, '/dist/index.html'), function(err) {
+    if (err) return res.status(500).send(err)
+  })
+})
 
+// Create default port to serve the app on
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 4000;
+app.listen(port)
 
-app.get('/api/helloworld', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Log a feedback that this is actually running
+console.log('Server started on port ' + port)
